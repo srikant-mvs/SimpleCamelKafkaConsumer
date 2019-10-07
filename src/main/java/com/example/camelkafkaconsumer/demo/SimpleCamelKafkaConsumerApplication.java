e2a2d6e07e84c82f8e518da6a018fdcd01ce0163
Srikant Mantha <srikant.mvs@gmail.com>
2019-10-07 14:44:54 +0200

    SimpleCamelKafkaConsumer project created

---
+++ src/main/java/com/example/camelkafkaconsumer/demo/SimpleCamelKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.camelkafkaconsumer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCamelKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCamelKafkaConsumerApplication.class, args);
+	}
+
+}
